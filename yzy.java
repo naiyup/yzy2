99facaf88f8f65ca125e30bf2d620db8b5739ec1
xxxxiaohu <101377763+xxxxiaohu@users.noreply.github.com>
2022-03-11 11:23:22 +0800

    Add files via upload

---
+++ yzy.java
@@ -0,0 +1,15 @@
+public class yzy {
+        public static void main(String[]args){
+        int x=0;
+        int y=0;
+        int z=0;
+        boolean a,b;
+        a= x > 0 & y++ > 1;
+        System.out.println(a);
+        System.out.println("y ="+y);
+        b = x > 0 && z++ > 1;
+        System.out.println(b);
+        System.out.println("z ="+z);
+
+    }
+}
